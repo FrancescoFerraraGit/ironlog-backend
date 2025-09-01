3ed3389ebcceb068558dd5caed3eb6c62f723316
FrancescoFerraraGit <f.ferrara993@gmail.com>
2025-09-01 15:28:32 +0200

    feat(backend): initial project setup with express

---
+++ server.js
@@ -0,0 +1,1 @@
+"const express = require('express');const app = express();const PORT = process.env.PORT || 5000;app.get('/', (req, res) => {  res.send('Server backend di IronLog in esecuzione!');});app.listen(PORT, () => {  console.log(\`Server in esecuzione sulla porta \${PORT}\`);});" 
